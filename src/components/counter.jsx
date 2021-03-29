0fdae2964bd4f2eaffc3cf5dec9a628cfe89f05a
nirzak <nirzashzakilim@gmail.com>
2021-03-29 16:03:51 +0600

    Fixed disalignment after increment and decrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,9 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };*/
     
+    styles = {
+        width: 50
+    }
 
     /*handleIncrement = () =>{
         this.setState({count: this.state.count + 1})
@@ -13,7 +16,7 @@ class Counter extends Component {
 
     render() {
         return (<div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-primary btn-sm m-2'>+</button>
                 <button disabled={this.getStatus()} onClick={() => this.props.onDecrement(this.props.counter)} className='btn btn-secondary btn-sm m-2'>-</button>
                 <button onClick={() => this.props.onDelete(this.props.counter.id)} className='btn btn-danger btn-sm m-2'>Delete</button>
