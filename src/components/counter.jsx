b260d0de9e8a73fb55d7e79ab0daae7a2f5cf761
nirzak <nirzashzakilim@gmail.com>
2021-03-29 13:08:35 +0600

    Initian Commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    /*state = {
+        count: this.props.counter.value,
+        tags: ['tag1', 'tag2', 'tag3']
+    };*/
+
+    /*styles = {
+        fontSize: 10,
+        fontWeight: "bold"
+    };*/
+
+    /*handleIncrement = () =>{
+        this.setState({count: this.state.count + 1})
+
+    }*/
+
+    render() {
+        return (<div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>Increament</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className='btn btn-danger btn-sm m-2'>Delete</button>
+                </div>);
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.props.counter.value == 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const { value } = this.props.counter;
+        return value ===0? 'Zero' : value;
+    }
+}
+ 
+export default Counter;
