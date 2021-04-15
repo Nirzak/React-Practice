a30c337e8d7d1412f192eb4a3c8d5695bf757cd1
nirzak <nirjas01@student.sust.edu>
2021-04-15 16:05:23 +0600

    Passing elements to react-dom

--- Another-react-app/src/index.js
+++ Another-react-app/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-   <React.StrictMode>
-   <App />
- </React.StrictMode>,
-  document.getElementById('root')
+const root = document.querySelector("#root");
+render(
+   //<React.StrictMode>
+   //<App />
+   React.createElement("h1", {}, "Online Supermarket"), root
+ //</React.StrictMode>,
+  //document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
